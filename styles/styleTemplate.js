765575adb8dad59c3f46ec7fcd493066280ad6a0
gordonmhy <gh@hpri.33mail.com>
2021-11-11 15:22:23 +0800

    Added some template files

---
+++ styles/styleTemplate.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from "react-native";
+
+export const componentStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
